d73a18402f50ef12e7824df9133008c94182ca5a
superdev <super.dev0714@gmail.com>
2019-11-14 23:02:29 -0800

    [UPDATED] importing success

--- app/elearning/admin.py
+++ app/elearning/admin.py
@@ -6,6 +6,9 @@ from question.models import Question, Answer
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 from import_export.results import Result
+from import_export.fields import Field
+from import_export.widgets import ForeignKeyWidget
+from import_export.widgets import CharWidget
 
 admin.site.register(Slide)
 admin.site.register(ELearningSession)
@@ -13,20 +16,43 @@ admin.site.register(ELearningUserSession)
 admin.site.register(ELearningUserAnswer)
 admin.site.register(ELearningCorrection)
 admin.site.register(ELearningRepetition)
-
-
+  
 class ELearningResource(resources.ModelResource):
+
+	name = Field(column_name='quiz')
+	category = Field(attribute='category', column_name='category')
+	sub_category = Field(column_name='sub_category')
+	figure = Field(column_name='figure')
+	content = Field(column_name='content')
+	explanation = Field(column_name='explanation')
+	correct = Field(column_name='correct')
+	answer1 = Field(column_name='answer1')
+	answer2 = Field(column_name='answer2')
+	answer3 = Field(column_name='answer3')
+	
 	class Meta:
 		model = ELearning
+		fields = ('name', 'category', 'sub_category', 'figure', 'content', 'explanation', 'correct', 'answer1', 'answer2', 'answer3')
 
-	def import_data(self, dataset, dry_run=False, raise_errors=False, *args, **kwargs):		
+	def import_data(self, dataset, dry_run=False, raise_errors=False, *args, **kwargs):
+		session_no = 1
+		if len(dataset) > 1:
+			prev_exam_name = dataset[0][1]
+			prev_figure = dataset[0][4]
 		for row in dataset:
 			exam_name = row[1]
 			q_category = row[2]
 			q_subcategory = row[3]
 			figure = row[4]
 			exam, crt = ELearning.objects.get_or_create(name=exam_name, exam_type=Exam.ELEARNING)
-			session_no = row[11]
+			if not figure == 'n' and prev_figure == 'n':
+				session_no += 1
+				print('decided session no: ', session_no, ' with exam name is: ', exam_name)
+			if not prev_exam_name == exam_name:
+				session_no = 1
+				print('session no is initialized as 1 with exam name: ', exam_name, 'prev name is: ', prev_exam_name)
+			prev_exam_name = exam_name
+			prev_figure = figure
 			session, crt = ELearningSession.objects.get_or_create(elearning=exam, number=session_no)
 			if not figure == 'n':
 				s, crt = Slide.objects.get_or_create(elearning=exam, image=figure)
@@ -59,5 +85,6 @@ class ELearningResource(resources.ModelResource):
 
 class ELearningAdmin(ImportExportModelAdmin):
 	resource_class = ELearningResource
+	# list_display = ('quiz', 'category')
 
 admin.site.register(ELearning, ELearningAdmin)
