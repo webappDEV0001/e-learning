7ef123e12fe63e05c45f5414578f91f0e295e329
xxbinxx <hi@bineetkumar.com>
2019-11-21 16:02:40 +0530

    Resolved issue elearning memory force key error

--- app/exam/templatetags/custom_filters.py
+++ app/exam/templatetags/custom_filters.py
@@ -5,12 +5,15 @@ from django.template.defaulttags import register
 @register.filter(name='memory_force_value')
 def memory_force_value(all_memory_force_values,current_exam_name):
     """Take memory force value dictionary and return current object value"""
-    memory_force_value = all_memory_force_values[current_exam_name]
-    if memory_force_value == "low":
-        color="red"
-    elif memory_force_value == "high":
-        color="green"
-    else:
+    try:
+        memory_force_value = all_memory_force_values[current_exam_name]
+        if memory_force_value == "low":
+            color="red"
+        elif memory_force_value == "high":
+            color="green"
+        else:
+            color="yellow"
+    except:
         color="yellow"
     return color
 
@@ -18,5 +21,9 @@ def memory_force_value(all_memory_force_values,current_exam_name):
 @register.filter(name='memory_force_title')
 def memory_force_title(all_memory_force_values,current_exam_name):
     """Take memory force value dictionary and return current object value"""
-    return all_memory_force_values[current_exam_name]
+    try:
+        title=all_memory_force_values[current_exam_name]
+    except:
+        title="Medium"
+    return title
 
