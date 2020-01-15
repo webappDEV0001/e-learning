54c90abbbb8d31f188900c1066ba26701fb00fb7
xxbinxx <hi@bineetkumar.com>
2020-01-15 10:33:27 +0530

    resolve merge conflict issue with 404 url

--- app/config/urls.py
+++ app/config/urls.py
@@ -46,7 +46,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
 
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
-         name='django.contrib.sitemaps.views.sitemap')
+         name='django.contrib.sitemaps.views.sitemap'),
     path('<slug:slug>',el_view.handler404)
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
