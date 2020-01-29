139d4ad08f55a4c8088a65c72a6c102047e8663a
xxbinxx <hi@bineetkumar.com>
2020-01-29 13:25:44 +0530

    slide swipe issue resolved

--- assets/js/presentation-slide.js
+++ assets/js/presentation-slide.js
@@ -110,4 +110,53 @@ if (check_current_slide == TOTAL_SLIDES){
     $("#start-exam-pre").css("visibility","hidden");
 }
 
+//swipe slide code
+var container = document.querySelector("#exam-screen");
+
+container.addEventListener("touchstart", startTouch, false);
+container.addEventListener("touchmove", moveTouch, false);
+
+// Swipe Up / Down / Left / Right
+var initialX = null;
+var initialY = null;
+
+function startTouch(e) {
+initialX = e.touches[0].clientX;
+initialY = e.touches[0].clientY;
+};
+
+function moveTouch(e) {
+if (initialX === null) {
+  return;
+}
+
+if (initialY === null) {
+  return;
+}
+
+var currentX = e.touches[0].clientX;
+var currentY = e.touches[0].clientY;
+
+var diffX = initialX - currentX;
+var diffY = initialY - currentY;
+
+if (Math.abs(diffX) > Math.abs(diffY)) {
+  // sliding horizontally
+  if (diffX > 0) {
+    // swiped left
+    //console.log("swiped left");
+     $("#start-exam-pre").click();
+  } else {
+    // swiped right
+     $("#previous_slide_pre").click();
+    //console.log("swiped right");
+
+  }
+}
+
+initialX = null;
+initialY = null;
+
+e.preventDefault();
+};
 
